c47d1d71456671dc0f0b4ff229e772b3bff33f1f
Evilcome <evilcome.com@gmail.com>
2015-04-06 21:50:56 +0800

    first commit

---
+++ grubby-worm-swift/Common.swift
@@ -0,0 +1,15 @@
+//
+//  Common.swift
+//  grubby-worm-swift
+//
+//  Created by Wayne on 15/4/6.
+//  Copyright (c) 2015年 Wayne. All rights reserved.
+//
+
+import Foundation
+
+enum Direction {
+    case North, South, East, West
+}
+
+// Type Struct
---
+++ grubby-worm-swift/Trigger.swift
@@ -0,0 +1,13 @@
+//
+//  Trigger.swift
+//  grubby-worm-swift
+//
+//  Created by Wayne on 15/4/6.
+//  Copyright (c) 2015年 Wayne. All rights reserved.
+//
+
+import Foundation
+
+class Trigger {
+    
+}
