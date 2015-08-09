311892c502df2e64fa12c765bf835ef2c0f23452
Wayne <Wayne@WaynedeMacBook-Pro.local>
2015-08-09 23:10:50 +0800

    change to ios9 sdk

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Grubby Worm Swift
 
 Grubby Worm的Swift版本，我在博客中有更详细的说明：[SWIFT.HOW BLOG](https://blog.swift.how)。
+
+你需要使用Xcode 7以上版本方能运行。
---
+++ grubby-worm-swift/DataStruct.swift
@@ -0,0 +1,9 @@
+//
+//  DataStruct.swift
+//  grubby-worm-swift
+//
+//  Created by Wayne on 15/8/9.
+//  Copyright © 2015年 SWIFT.HOW. All rights reserved.
+//
+
+import Foundation
